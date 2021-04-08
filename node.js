fabfc39ef7780de3feb6ee0163c292e7d03a3bbe
PatrykSroka21 <patryk.sroka01@gmail.com>
2021-04-08 12:57:16 +0200

    new skit

--- node.js
+++
@@ -1,8 +0,0 @@
-const const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('first request'))
-
-
-app.listen(port, () => console.log(`Example app listening on port port!`))
